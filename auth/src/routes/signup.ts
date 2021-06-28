4dffda7fcc5301ddf5b0e973551ae3027c353129
Audifaz <emilio.aguila@hotmail.com>
2021-06-28 13:11:37 -0500

    feat(auth): Authentication Service is now complete

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, {Request,Response} from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
+
 
 const router = express.Router();
 
@@ -14,12 +16,8 @@ router.post('/api/users/signup', [
      .trim()
      .isLength({min: 4, max: 20})
      .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request,res: Response) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
-    }
+], validateRequest
+,async (req: Request,res: Response) => {
 
     const {email,password} = req.body;
 
@@ -33,9 +31,16 @@ router.post('/api/users/signup', [
 
     await user.save();
 
-    res.status(201).send(user);
+    // Generate JWT and store it on session object
+    const userJWT = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY! );
 
-    //return res.send({});
+    req.session = { jwt: userJWT };
+
+
+    res.status(201).send(user);
 });
 
 export {router as signupRouter};
