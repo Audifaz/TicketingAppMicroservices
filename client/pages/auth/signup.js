6137e5c39d20a626d63ec8ea77cb66a96f9d2811
Audifaz <emilio.aguila@hotmail.com>
2021-07-01 10:06:54 -0500

    Tests

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+export default () => {
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(email,password);
+    }
+
+    return (<form onSubmit={onSubmit}>
+        <h1>Sign up</h1>
+        <div className="form-group">
+            <label>Email Address</label>
+            <input value={email} onChange={e=>setEmail(e.target.value)} className="form-group"/>
+        </div>
+        <div className="form-group">
+            <label>Password</label>
+            <input value={password} onChange={e=> setPassword(e.target.value)} type="password" className="form-group"/>
+        </div>
+        <button className="btn btn-primary">Sign Up</button>
+    </form>);
+};
