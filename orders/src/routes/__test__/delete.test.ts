520dcd9b6dca1168244e2c22847c8d144deaa567
Audifaz <emilio.aguila@hotmail.com>
2021-07-15 20:06:20 -0500

    feat(Nats): Events for the Orders service now work

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import {app} from '../../app';
 import mongoose from 'mongoose';
 import {Order, OrderStatus} from '../../models/order';
 import {Ticket} from '../../models/ticket';
+import {natsWrapper} from '../../nats-wrapper';
 
 it('marks an order as cancelled', async () => {
     //cancelled a ticket with Ticket Model
@@ -31,4 +32,26 @@ it('marks an order as cancelled', async () => {
     expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('emits an order cancelled event');
+it('emits an order cancelled event', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user = global.signin();
+    //Make a request to create an order
+    const {body: order} =await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ticketId: ticket.id})
+    .expect(201);
+    //make a request to cancel the order
+    await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie',user)
+    .send()
+    .expect(204);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
